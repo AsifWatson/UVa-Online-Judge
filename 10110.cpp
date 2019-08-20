173d21733e1af7f8a1f74b6646dbc7d0111092f2
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-08-20 11:46:23 +0600

    Add files via upload

---
+++ 10110.cpp
@@ -0,0 +1,62 @@
+/***********************************************************|
+|          /\           /-------|   -----    |-----------|  |
+|         /  \         /              |      |              |
+|        /    \       |               |      |              |
+|       /      \      |----|          |      |-------|      |
+|      / ------ \          |-----|    |      |              |
+|     /          \               |    |      |              |
+|    /            \              /    |      |              |
+|  ---            --- |---------/   -----  -----            |
+|                                                           |
+|   Codeforces = Asif_Watson  ||  Codechef = asif_watson    |
+|   Mail = ashfaqislam33@gmail.com                          |
+|   CSTE, Noakhali Science and Technology University(11)    |
+|                                                           |
+************************************************************/
+
+#include "bits/stdc++.h"
+#define all(v) v.begin(),v.end()
+#define allre(v) v.rbegin(),v.rend()
+#define sp(x,y) fixed<<setprecision(y)<<x
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define GCD(a,b) __gcd(a,b)
+#define LCM(a,b) ((a*b)/__gcd(a,b))
+
+using namespace std;
+
+const double pi = acos(-1.0);
+const double EPS = 1e-6;
+const int MOD = (int)1e9+7;
+
+bool Reverse(long long a,long long b){return a>b;}
+
+bool compare(pair<long long,long long> a,pair<long long,long long> b)
+{
+    if(a.first==b.first)return a.second>b.second;
+    return a.first<b.first;
+}
+
+bool ok(long long n)
+{
+    long long a=sqrt(n);
+    a*=a;
+    if(a==n)return true;
+    return false;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    long long n;
+    while(cin>>n)
+    {
+        if(n==0)break;
+        if(ok(n))cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+
+    return 0;
+}
+
